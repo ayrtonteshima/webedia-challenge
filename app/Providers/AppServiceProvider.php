0d6deb5b39155fc4c5f250e913215b718ea437f5
ayrtonteshima <ayrtonteshima@gmail.com>
2017-02-27 19:59:59 -0300

    Bind PostRepositorie when PostInterface

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app->bind(
+            'Webedia\Repositories\Contracts\PostInterface',
+            'Webedia\Repositories\PostRepositorie'
+        );
     }
 
     /**
